2d6576498c271efc9c475bfbe028d2f6f44215b7
Kuntamon <avelakop@gmail.com>
2022-05-21 04:04:10 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,43 +19,42 @@ def main():
     print(kmldoc)
 
     def plotIPs(pcap):
-        kmlPts = ''
-    for (ts, buf) in pcap:
+            kmlPts = ''
+            for (ts, buf) in pcap:
+                try:
+                    eth = dpkt.ethernet.Ethernet(buf)
+                    ip = eth.data
+                    src = socket.inet_ntoa(ip.src)
+                    dst = socket.inet_ntoa(ip.dst)
+                    KML = retKML(dst, src)
+                    kmlPts = kmlPts + KML
+                except:
+                    pass
+            return kmlPts
+
+    def retKML(dstip, srcip):
+        dst = gi.record_by_name(dstip)
+        src = gi.record_by_name('your.ip.here')
         try:
-            eth = dpkt.ethernet.Ethernet(buf)
-            ip = eth.data
-            src = socket.inet_ntoa(ip.src)
-            dst = socket.inet_ntoa(ip.dst)
-            KML = retKML(dst, src)
-            kmlPts = kmlPts + KML
-        except:
-            pass
-    return kmlPts
-
-def retKML(dstip, srcip):
-    dst = gi.record_by_name(dstip)
-    src = gi.record_by_name('your.ip.here')
-    try:
-        dstlongitude = dst['longitude']
-        dstlatitude = dst['latitude']
-        srclongitude = src['longitude']
-        srclatitude = src['latitude']
-        kml = (
-            '<Placemark>\n'
-            '<name>%s</name>\n'
-            '<extrude>1</extrude>\n'
-            '<tessellate>1</tessellate>\n'
-            '<styleUrl>#transBluePoly</styleUrl>\n'
-            '<LineString>\n'
-            '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
-            '</LineString>\n'
-            '</Placemark>\n'
+            dstlongitude = dst['longitude']
+            dstlatitude = dst['latitude']
+            srclongitude = src['longitude']
+            srclatitude = src['latitude']
+            kml = (
+                '<Placemark>\n'
+                '<name>%s</name>\n'
+                '<extrude>1</extrude>\n'
+                '<tessellate>1</tessellate>\n'
+                '<styleUrl>#transBluePoly</styleUrl>\n'
+                '<LineString>\n'
+                '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
+                '</LineString>\n'
+                '</Placemark>\n'
         )%(dstip, dstlongitude, dstlatitude, srclongitude, srclatitude)
-        return kml
-    except:
-        return ''    
+            return kml
+        except:
+            return ''    
 
 
-if __name__ == '__main__':
-    main()
-    
+    if __name__ == '__main__':
+        main()
